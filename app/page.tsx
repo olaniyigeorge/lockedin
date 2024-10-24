1ccbe317f0b8da5fd87602101a49d0103caa0b60
Onimisea <onimisea@gmail.com>
2024-10-24 19:20:31 +0100

    LockedIn App scaffold...

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)] gap-4 px-20 bg-[#fafafa] text-center">
+      <h1 className="font-bold text-4xl">
+        <span className="text-green-700">Locked</span>
+        <span className="text-orange-500">In</span>
+      </h1>
+      <p className="text-gray-700">
+        No. 1 Habit and Routine Tracking App in the world...
+      </p>
+    </div>
+  );
+}
