f159f219e6a0988329e105ec073cf7da6e9b5f16
Onimisea <onimisea@gmail.com>
2024-11-06 09:53:13 +0100

    .

--- app/life-domains/[id]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-
-export default async function NotFound({ params }: { params: { id: string } }) {
-
-    return (
-        <div className="">
-            Life Domain with id {params.id} not found
-        </div>
-    )
-}
