9fb4444c4167b81f40d2af3844360c2c6841a03e
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-01-11 03:29:07 +0100

    new e2e

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,48 @@
-import User from "@/models/user";
-import { connectToDB } from "@/utils/db.service.mongo";
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+// import User from "@/models/user";
+// import { connectToDB } from "@/utils/db.service.mongo";
+// import NextAuth from "next-auth";
+// import Google from "next-auth/providers/google";
 
-const handler = NextAuth({
-  providers: [
-    Google
-  ],
-  callbacks: {
-    async session({ session }) {
-      // store the user id from MongoDB to session
+// const handler = NextAuth({
+//   providers: [
+//     Google
+//   ],
+//   callbacks: {
+//     async session({ session }) {
+//       // store the user id from MongoDB to session
 
-      return session;
-    },
-    async signIn({ account, profile, user, credentials }) {
-      try {
-        await connectToDB();
+//       return session;
+//     },
+//     async signIn({ account, profile, user, credentials }) {
+//       try {
+//         await connectToDB();
 
-        // Check if profile is defined
-        if (!profile || !profile.email) {
-            console.error('No profile or email found');
-            return false;
-            }
+//         // Check if profile is defined
+//         if (!profile || !profile.email) {
+//             console.error('No profile or email found');
+//             return false;
+//             }
 
-        // // check if user already exists
-        // const userExists = await User.findOne({ email: profile?.email });
+//         // // check if user already exists
+//         // const userExists = await User.findOne({ email: profile?.email });
 
-        // // if not, create a new document and save user in MongoDB
-        // if (!userExists) {
-        //     const username = profile.name?.replace(" ", "").toLowerCase() || "user"; // Fallback if name is undefined
-        //   await User.create({
-        //     email: profile?.email,
-        //     username: username,
-        //     image: profile?.picture || "",
-        //   });
-        // }
+//         // // if not, create a new document and save user in MongoDB
+//         // if (!userExists) {
+//         //     const username = profile.name?.replace(" ", "").toLowerCase() || "user"; // Fallback if name is undefined
+//         //   await User.create({
+//         //     email: profile?.email,
+//         //     username: username,
+//         //     image: profile?.picture || "",
+//         //   });
+//         // }
 
-        return true
-      } catch (error) {
-        console.log("Error checking if user exists: ", error);
-        return false
-      }
-    },
-  }
-})
+//         return true
+//       } catch (error) {
+//         console.log("Error checking if user exists: ", error);
+//         return false
+//       }
+//     },
+//   }
+// })
 
-export { handler as GET, handler as POST }
+// export { handler as GET, handler as POST }
