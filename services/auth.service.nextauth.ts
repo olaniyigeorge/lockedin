cefacfd897c2ecbc9d47c739530c8af74930b9b3
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-01-11 04:01:21 +0100

    fdis

--- services/auth.service.nextauth.ts
+++ services/auth.service.nextauth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
-import { connectToDB } from "./db.service.mongo"
+import { connectToDB } from "./db_mongo"
 import User from "@/models/user"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
