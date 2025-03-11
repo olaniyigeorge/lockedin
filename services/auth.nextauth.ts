f4f0b5e2e9807142731989b50daba93f72d8668f
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-03-11 15:56:33 +0100

    soc

--- services/auth.nextauth.ts
+++ services/auth.nextauth.ts
@@ -1,3 +1,149 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { encrypt } from "@/utils/helpers";
+import { AuthenticatedUser, AuthPayload, AuthResponse } from "@/app/api/auth/signin/docs";
+// import { AuthenticatedUser, AuthPayload, AuthResponse } from "./docs";
+
+const DOMAIN = process.env.DOMAIN as string;
+const SESSION_VALIDITY_DURATION_IN_MINUTES = process.env
+    .SESSION_VALIDITY_DURATION_IN_MINUTES as string;
+
+
+
+
+/**
+ * ======================================================
+ *                      SIGN-OUT 
+ * ======================================================
+ */
+
+export async function signout(request: NextRequest): Promise<NextResponse> {
+    /**
+     * Wipes session from cookies and removes session from header
+     */
+    const sessionCookie = await cookies();
+
+    // Clear session cookie
+    sessionCookie.set("session", "", {
+        maxAge: -1,
+        httpOnly: true,
+        path: "/",
+        secure: true,
+    });
+
+    // Create response
+    const response = NextResponse.json(
+        { message: "Sign-out successful" },
+        { status: 200 }
+    );
+
+    // Remove token from header
+    response.headers.delete("token");
+
+    return response;
+}
+
+
+
+
+/**
+ * ======================================================
+ *                      SIGN-IN 
+ * ======================================================
+ */
+export async function signin(
+  req: NextRequest
+): Promise<NextResponse<AuthResponse>> {
+  const req_body: AuthPayload = await req.json();
+  const sessionCookie = await cookies();
+  try {
+    const res = await fetch(`${DOMAIN}/api/users/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: req_body.email,
+        password: req_body.password,
+      }),
+    });
+
+    if (!res.ok) {
+      const resp = await res.json()
+      return NextResponse.json(
+        { error: `${resp.error}` },
+        { status: res.status }
+      );
+    }
+    const data = await res.json();
+    const cleanedUser: AuthenticatedUser = {
+      _id: data.user._id,
+      email: data.user.email,
+      first_name: data.user.first_name,
+      last_name: data.user.last_name,
+      username: data.user.username,
+      image: data.user.image,
+      isVerified: data.user.isVerified,
+      role: data.user.role,
+    };
+    const session = await encrypt({
+      expires: new Date(
+        Date.now() + parseInt(SESSION_VALIDITY_DURATION_IN_MINUTES) * 60 * 1000
+      ),
+      user: {
+        email: data.user.email,
+        username: data.user.username,
+        role: data.user.role,
+        id: data.user._id,
+      },
+    });
+
+    // Set  session cookie
+    sessionCookie.set("session", session, {
+      maxAge: parseInt(SESSION_VALIDITY_DURATION_IN_MINUTES) * 60,
+      httpOnly: true,
+      path: "/",
+      secure: true,
+    });
+
+ 
+    // You CANNOT set headers on `req` directly.
+    // Instead, set headers on your response:
+    const response = NextResponse.json(
+      {
+        message: "Authentication Successfull",
+        user: cleanedUser,
+      },
+      { status: 200 }
+    );
+    // Set token in header
+    response.headers.set("token", session);
+
+    return response
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { error: `${error}` },
+      { status: 500 }
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import NextAuth from "next-auth"
 // import Google from "next-auth/providers/google"
 // import GitHub from "next-auth/providers/github"
